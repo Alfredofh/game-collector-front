6c646f7db993bdd6a12fa05fb2f3645bbcf330d6
DESKTOP-7T5FV20\alfre <alfredofweb@gmail.com>
2024-09-15 20:42:20 +0200

    feat(Login): Added react-router-dom for client-side routing, introduced LoginForm component, and created loginUser function in userServices

--- src/services/userServices.ts
+++ src/services/userServices.ts
@@ -6,7 +6,19 @@ interface RegisterUserData {
     password: string;
 }
 
+interface LoginUserData {
+    email: string;
+    password: string;
+}
+
+
 export const registerUser = async (userData: RegisterUserData) => {
     const response = await axios.post('/users', userData);
     return response.data;
 };
+
+
+export const loginUser = async (userData: LoginUserData) => {
+    const response = await axios.post('/api/login', userData);
+    return response.data;
+}
