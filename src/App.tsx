02ccb312a7dcfb5dfa1b32cb928b2bb3c7d36225
DESKTOP-7T5FV20\alfre <alfredofweb@gmail.com>
2024-09-17 18:31:03 +0200

    feat(LogOut): Added Logout component.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import RegisterForm from './components/RegisterForm';
 import LoginForm from './components/LoginForm';
 import GlobalStyles from './styles/GlobalStyles'; 
+import Logout from './components/LogOut';
 
 const App: React.FC = () => {
     return (
