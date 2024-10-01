182fed2f2359c00964772480717cea1c5c57c7dc
DESKTOP-7T5FV20\alfre <alfredofweb@gmail.com>
2024-10-01 18:49:46 +0200

    feat(Menu): Added useAuth hook and logout function to LogOut component, removed token from localStorage.

--- src/components/LogOut.tsx
+++ src/components/LogOut.tsx
@@ -1,13 +1,21 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../contexts/authContext'; 
 
 const Logout: React.FC = () => {
     const navigate = useNavigate();
+    const { logout } = useAuth(); 
 
     useEffect(() => {
+        // Remove the token from localStorage
         localStorage.removeItem('token');
+        
+        // Call the context's logout function to update the app state
+        logout();
+        
+        // Redirect the user to the login page
         navigate('/login');
-    }, [navigate]);
+    }, [navigate, logout]);
 
     return null; 
 };
