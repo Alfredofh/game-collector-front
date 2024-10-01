b9a4d9d6b1ec11cbf5f5c66f19a76b492b55e64e
DESKTOP-7T5FV20\alfre <alfredofweb@gmail.com>
2024-10-01 18:44:04 +0200

    feat(NavBar): Added NavBar component to App.tsx and added logout route

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,26 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import RegisterForm from './components/RegisterForm';
 import LoginForm from './components/LoginForm';
-import GlobalStyles from './styles/GlobalStyles'; 
+import GlobalStyles from './styles/GlobalStyles';
 import Logout from './components/LogOut';
 import UserDashboard from './pages/UserDashboard';
 import { AuthProvider } from './contexts/authContext';
+import NavBar from './components/NavBar'; 
+
 const App: React.FC = () => {
     return (
         <>
             <GlobalStyles /> 
             <AuthProvider>
-            <Router>
-                <Routes>
-                    <Route path="/register" element={<RegisterForm />} />
-                    <Route path="/login" element={<LoginForm />} />
-                    <Route path="/dashboard" element={<UserDashboard />} />
-                </Routes>
-            </Router>
+                <Router>
+                    <NavBar />
+                    <Routes>
+                        <Route path="/register" element={<RegisterForm />} />
+                        <Route path="/login" element={<LoginForm />} />
+                        <Route path="/dashboard" element={<UserDashboard />} />
+                        <Route path="/logout" element={<Logout />} /> 
+                    </Routes>
+                </Router>
             </AuthProvider>
         </>
     );
