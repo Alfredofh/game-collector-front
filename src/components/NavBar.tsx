0169de984b467a67f4f13434c91380e3bdf0b498
DESKTOP-7T5FV20\alfre <alfredofweb@gmail.com>
2025-01-17 20:34:39 +0100

    refactor(menu): add menu for non-logged in user, so that he/she can log in or register. The menu is hidden for non-logged in users.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,33 +1,48 @@
 // src/components/NavBar.tsx
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import { useAuth } from '../contexts/authContext';
 
 const NavBar: React.FC = () => {
+    const { isAuthenticated, logout } = useAuth();
     return (
         <Nav>
             <NavList>
-                <NavItem>
-                    <StyledLink to="/dashboard">Home</StyledLink>
-                </NavItem>
-                <NavItem>
-                    <StyledLink to="/collection">My Collections</StyledLink>
-                </NavItem>
-                <NavItem>
-                    <StyledLink to="/collection/new">New Collection</StyledLink>
-                </NavItem>
-                <NavItem>
-                    <StyledLink to="/scanner">Search Games</StyledLink>
-                </NavItem>
-                <NavItem>
-                    <StyledLink to="/prices">Prices</StyledLink>
-                </NavItem>
-                <NavItem>
-                    <StyledLink to="/profile">Profile</StyledLink>
-                </NavItem>
-                <NavItem>
-                    <StyledLink to="/logout">Logout</StyledLink>
-                </NavItem>
+                {isAuthenticated ? (
+                    <>
+                        <NavItem>
+                            <StyledLink to="/dashboard">Home</StyledLink>
+                        </NavItem>
+                        <NavItem>
+                            <StyledLink to="/collection">My Collections</StyledLink>
+                        </NavItem>
+                        <NavItem>
+                            <StyledLink to="/collection/new">New Collection</StyledLink>
+                        </NavItem>
+                        <NavItem>
+                            <StyledLink to="/scanner">Search Games</StyledLink>
+                        </NavItem>
+                        <NavItem>
+                            <StyledLink to="/prices">Prices</StyledLink>
+                        </NavItem>
+                        <NavItem>
+                            <StyledLink to="/profile">Profile</StyledLink>
+                        </NavItem>
+                        <NavItem>
+                            <StyledButton onClick={logout}>Logout</StyledButton>
+                        </NavItem>
+                    </>
+                ) : (
+                    <>
+                        <NavItem>
+                            <StyledLink to="/login">Login</StyledLink>
+                        </NavItem>
+                        <NavItem>
+                            <StyledLink to="/register">Register</StyledLink>
+                        </NavItem>
+                    </>
+                )}
             </NavList>
         </Nav>
     );
@@ -59,5 +74,18 @@ const StyledLink = styled(Link)`
         color: #1b9aaa;
     }
 `;
+
+const StyledButton = styled.button`
+    background: none;
+    border: none;
+    color: #ffffff;
+    font-family: 'Press Start 2P', cursive;
+    cursor: pointer;
+
+    &:hover {
+        color: #1b9aaa;
+    }
+`;
+
 
 export default NavBar;
